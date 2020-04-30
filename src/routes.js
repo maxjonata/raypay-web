e516aa04b2705ca565c85c98d14245bd00a997b6
JAMaxwell <max.kawaii@hotmail.com>
2020-04-30 17:36:26 -0300

    Ativação do fullscreen

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,8 @@ import QRReader from './pages/QRReader';
 import ObjectTalkBack from './pages/ObjectTalkBack';
 import Confirmation from './pages/Confirmation';
 import Start from './pages/TestAlertPage';
+import { useRef } from 'react';
+import { useEffect } from 'react';
 /*
 import Loading from './pages/Loading';
 import ObjectTalkBack from './pages/ObjectTalkBack';
@@ -14,6 +16,11 @@ import Confirmation from './pages/Confirmation';
 <Route path="/information/confirm" component={Confirmation}/>
 */
 export default function Routes() {
+    const screenref = useRef(null);
+    useEffect(() => {
+        screenref.current.requestFullscreen();
+    })
+
     return(
         <BrowserRouter>
             <Switch>
