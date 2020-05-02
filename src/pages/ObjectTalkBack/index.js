ac7496e115a7abbf9d4a1f511330e852ad390e6d
JAMaxwell <max.kawaii@hotmail.com>
2020-05-02 15:30:04 -0300

    removido fullscreen da janela /information

--- src/pages/ObjectTalkBack/index.js
+++ src/pages/ObjectTalkBack/index.js
@@ -1,19 +1,13 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import AccessibilityDataList from './../../assets/Componentes/AccessibilityDataList';
 import Choices from './../../assets/Componentes/Choices';
 
 import './styles.css';
 import mp3_file from '../../assets/Sounds/bleepRead.mp3';
-import { useRef } from 'react';
 
 export default function ObjectTalkBack() {
-    const screenref = useRef(null);
-    useEffect(() => {
-        screenref.current.requestFullscreen();
-    })
-
-    return(
+    return (
         <div className="objTalkBack-container" ref={screenref}>
             <audio id="audio" src={mp3_file} autoPlay/>
             <AccessibilityDataList dataString={localStorage.getItem('data')}/>
